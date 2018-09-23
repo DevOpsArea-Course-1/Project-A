bdb4800661de993b6e14b7b06a5c6ce9845ff345
Mohamed Zeineldin <eng.mohamed.zeineldin@gmail.com>
2018-09-23 22:41:56 +0200

    Create server.js

---
+++ Nodejs-express-testapp-1/server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+// Constants
+const PORT = 7777;
+const HOST = '0.0.0.0';
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('App  1 Is Working :) \n');
+});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
